7e4b3a0973de82fc0932e501add894ee9ab588ab
ludste <ludste@kth.se>
2014-02-28 12:20:56 +0100

    Test http response

--- TaskTracker/src/com/stenstrom/TaskTracker/MyActivity.java
+++ TaskTracker/src/com/stenstrom/TaskTracker/MyActivity.java
@@ -2,6 +2,19 @@ package com.stenstrom.TaskTracker;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.TextView;
+import org.apache.http.HttpResponse;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.message.BasicNameValuePair;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class MyActivity extends Activity {
     /**
@@ -11,5 +24,29 @@ public class MyActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+
+
+        // Create a new HttpClient and Post Header
+        HttpClient httpclient = new DefaultHttpClient();
+        HttpPost httppost = new HttpPost("http://ludste.synology.me");
+
+        try {
+            // Add your data
+            List<NameValuePair> nameValuePairs = new ArrayList<NameValuePair>(2);
+            nameValuePairs.add(new BasicNameValuePair("id", "12345"));
+            nameValuePairs.add(new BasicNameValuePair("stringdata", "AndDev is Cool!"));
+            httppost.setEntity(new UrlEncodedFormEntity(nameValuePairs));
+
+            // Execute HTTP Post Request
+            HttpResponse response = httpclient.execute(httppost);
+            TextView tv = (TextView) findViewById(R.id.Text);
+            tv.append("Hej");
+            tv.append("\n" + response.toString());
+            tv.append("Done");
+        } catch (ClientProtocolException e) {
+            // TODO Auto-generated catch block
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+        }
     }
 }
