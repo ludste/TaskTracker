8e2a0e703f0e41254100bd1d3944bd3446cab59a
ludste <ludste@kth.se>
2014-03-23 20:31:15 +0100

    Added pomodoro timer

--- TaskTracker/src/com/stenstrom/TaskTracker/Constants.java
+++ TaskTracker/src/com/stenstrom/TaskTracker/Constants.java
@@ -1,42 +1,48 @@
 package com.stenstrom.TaskTracker;
 
 public class Constants {
-	public static String add = "addTask";
-	public static String remove = "removeTask";
-	public static String setDone = "setDone";
-	public static String getTasks = "getUserTasks";
-	public static String getAllTasks = "getAllTasks";
-	public static String checkUserUniqueness = "testUserUniqueness";
-	public static String checkMailUniqueness = "testMailUniqueness";
-	public static String updatePomodoro = "updatePomodoro";
-	public static String addCollaborator = "addCollaborator";
-	public static String authenticate = "authenticateUser";
-	public static String signUp = "addUser";
-	public static String getCollab = "getCollaborators";
-	
-	public static final String METHOD = "method";
-	public static final String STATUS = "status";
-	public static final String DATA = "data";
-	
-	
-	public static final String USER_ID = "user_id";
-	public static final String USER_ID_DB = "user_id";
-	public static final String TASK_NAME = "name";
-	public static final String START_TIME = "start_date";
-	public static final String END_TIME = "end_date";
-	public static final String COMPLETED_WHOLE_TASK_DATE = "done_date";
-	public static final String NUM_OF_POMODOROS = "pomodoros";
-	public static final String IS_COLLABORATIVE = "collaborative";
-	public static final String TASK_ID_DB = "task_id";
-	public static final String IS_COMPLETED = "is_done";
-	public static final String NUM_COMPLETED_POMODOROS = "comp_pomodoros";
-	public static final String OWN_POMODOROS = "counter";
-	
-	public static final String username = "username";
-	public static final String password = "password";
-	public static final String email = "email";
-	
-	public static final String COLLAB = "collaborators";
-	
-	public static final String CONTACT_MAP = "contact";
+    public static String add = "addTask";
+    public static String remove = "removeTask";
+    public static String setDone = "setDone";
+    public static String getTasks = "getUserTasks";
+    public static String getAllTasks = "getAllTasks";
+    public static String checkUserUniqueness = "testUserUniqueness";
+    public static String checkMailUniqueness = "testMailUniqueness";
+    public static String updatePomodoro = "updatePomodoro";
+    public static String addCollaborator = "addCollaborator";
+    public static String authenticate = "authenticateUser";
+    public static String signUp = "addUser";
+    public static String getCollab = "getCollaborators";
+
+    public static final String METHOD = "method";
+    public static final String STATUS = "status";
+    public static final String DATA = "data";
+
+
+    public static final String USER_ID = "user_id";
+    public static final String USER_ID_DB = "user_id";
+    public static final String TASK_NAME = "name";
+    public static final String START_TIME = "start_date";
+    public static final String END_TIME = "end_date";
+    public static final String COMPLETED_WHOLE_TASK_DATE = "done_date";
+    public static final String NUM_OF_POMODOROS = "pomodoros";
+    public static final String IS_COLLABORATIVE = "collaborative";
+    public static final String TASK_ID_DB = "task_id";
+    public static final String IS_COMPLETED = "is_done";
+    public static final String NUM_COMPLETED_POMODOROS = "comp_pomodoros";
+    public static final String OWN_POMODOROS = "counter";
+
+    public static final String username = "username";
+    public static final String password = "password";
+    public static final String email = "email";
+
+    public static final String COLLAB = "collaborators";
+
+    public static final String CONTACT_MAP = "contact";
+
+    public static final String TIME_POMODORO = "pomodoro";
+    public static final String TIME_SHORT = "short";
+    public static final String TIME_LONG = "long";
+
+
 }
--- TaskTracker/src/com/stenstrom/TaskTracker/SingleTask.java
+++ TaskTracker/src/com/stenstrom/TaskTracker/SingleTask.java
@@ -1,177 +1,224 @@
 package com.stenstrom.TaskTracker;
 
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.os.Handler;
+import android.util.Log;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+import org.apache.http.NameValuePair;
+import org.apache.http.message.BasicNameValuePair;
+import org.json.JSONObject;
+
 import java.util.ArrayList;
 import java.util.HashMap;
 
-import org.apache.http.NameValuePair;
-import org.apache.http.message.BasicNameValuePair;
-import org.json.JSONArray;
-import org.json.JSONObject;
-import org.w3c.dom.Text;
-
-import android.app.Activity;
-import android.app.AlertDialog;
-import android.content.Context;
-import android.content.DialogInterface;
-import android.content.Intent;
-import android.graphics.drawable.Drawable;
-import android.os.AsyncTask;
-import android.os.Bundle;
-import android.util.Log;
-import android.view.View;
-import android.view.Window;
-import android.widget.ImageView;
-import android.widget.LinearLayout;
-import android.widget.TextView;
-
 public class SingleTask extends Activity {
-	HashMap<String, String> contactMap;
-	String collaborators;
-	int userID;
+    HashMap<String, String> contactMap;
+    String collaborators;
+    int userID;
 
-	@Override
-	protected void onCreate(android.os.Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		userID = getSharedPreferences(getString(R.string.preference_key_file), 0).getInt(
-				Constants.USER_ID, -1);
-		try {
+    @Override
+    protected void onCreate(android.os.Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        userID = getSharedPreferences(getString(R.string.preference_key_file), 0).getInt(
+                Constants.USER_ID, -1);
+        try {
 
-			setContentView(R.layout.single_task);
+            setContentView(R.layout.single_task);
 
-			String taskName;
-			String endTime;
-			String pomodoros;
-			String completedPom;
-			String isCollab;
-			String completedByMe;
-			Bundle extras = getIntent().getExtras();
-			if (extras != null) {
-				contactMap = (HashMap<String, String>) extras.get(Constants.CONTACT_MAP);
-				taskName = contactMap.get(Constants.TASK_NAME);// extras.getString(Constants.TASK_NAME);
-				endTime = contactMap.get(Constants.END_TIME);
-				pomodoros = contactMap.get(Constants.NUM_OF_POMODOROS);
-				completedPom = contactMap.get(Constants.NUM_COMPLETED_POMODOROS);
-				isCollab = contactMap.get(Constants.IS_COLLABORATIVE);
-				completedByMe = contactMap.get(Constants.OWN_POMODOROS);
+            String taskName;
+            String endTime;
+            String pomodoros;
+            String completedPom;
+            String isCollab;
+            String completedByMe;
+            Bundle extras = getIntent().getExtras();
+            if (extras != null) {
+                contactMap = (HashMap<String, String>) extras.get(Constants.CONTACT_MAP);
+                taskName = contactMap.get(Constants.TASK_NAME);// extras.getString(Constants.TASK_NAME);
+                endTime = contactMap.get(Constants.END_TIME);
+                pomodoros = contactMap.get(Constants.NUM_OF_POMODOROS);
+                completedPom = contactMap.get(Constants.NUM_COMPLETED_POMODOROS);
+                isCollab = contactMap.get(Constants.IS_COLLABORATIVE);
+                completedByMe = contactMap.get(Constants.OWN_POMODOROS);
 
-				TextView nameView = (TextView) findViewById(R.id.task_name_s);
-				TextView endView = (TextView) findViewById(R.id.end_s);
-				TextView pomView = (TextView) findViewById(R.id.pomodoros_s);
-				TextView completedView = (TextView) findViewById(R.id.pomodoros_comp_tot);
-				TextView completedByMeView = (TextView) findViewById(R.id.pomodoros_comp_me);
-				// TextView collabView =
-				// (TextView)findViewById(R.id.collaborate);
+                TextView nameView = (TextView) findViewById(R.id.task_name_s);
+                TextView endView = (TextView) findViewById(R.id.end_s);
+                TextView pomView = (TextView) findViewById(R.id.pomodoros_s);
+                TextView completedView = (TextView) findViewById(R.id.pomodoros_comp_tot);
+                TextView completedByMeView = (TextView) findViewById(R.id.pomodoros_comp_me);
+                // TextView collabView =
+                // (TextView)findViewById(R.id.collaborate);
 
-				nameView.setText(taskName);
-				endView.setText(endTime);
-				pomView.setText(pomodoros);
-					completedView.setText(completedPom);
-					completedByMeView.setText(completedByMe);
-				if (isCollab.equals("1")) {
-					ImageView i = (ImageView) findViewById(R.id.collab_image);
-					i.setVisibility(View.VISIBLE);
-					
-					String taskID = contactMap.get(Constants.TASK_ID_DB);
-					SetWithDB dbConn = new SetWithDB(Constants.getCollab, taskID, null);
-					dbConn.execute().get();
-					((TextView) findViewById(R.id.single_task_TV_shared_text)).setVisibility(View.VISIBLE);
-					TextView collab = (TextView) findViewById(R.id.single_task_TV_shared);
-					collab.setText(collaborators);
-					collab.setVisibility(View.VISIBLE);
+                nameView.setText(taskName);
+                endView.setText(endTime);
+                pomView.setText(pomodoros);
+                completedView.setText(completedPom);
+                completedByMeView.setText(completedByMe);
+                if (isCollab.equals("1")) {
+                    ImageView i = (ImageView) findViewById(R.id.collab_image);
+                    i.setVisibility(View.VISIBLE);
+
+                    String taskID = contactMap.get(Constants.TASK_ID_DB);
+                    SetWithDB dbConn = new SetWithDB(Constants.getCollab, taskID, null);
+                    dbConn.execute().get();
+                    ((TextView) findViewById(R.id.single_task_TV_shared_text)).setVisibility(View.VISIBLE);
+                    TextView collab = (TextView) findViewById(R.id.single_task_TV_shared);
+                    collab.setText(collaborators);
+                    collab.setVisibility(View.VISIBLE);
 //					TextView t = new TextView(this);
 //					t.setText("these are the collaborators: " + collaborators);
 //					LinearLayout singleView = (LinearLayout) findViewById(R.id.single_task);
 //					singleView.addView(t);
 
-				}
+                }
 
-			}
 
-		}
-		catch (Exception e) {
-			Log.e("Error", "Error in: " + e.getMessage());
-			e.printStackTrace();
-		}
-	}
+            }
 
-	public void registerPomodoro(View view) {
-		SetWithDB dbConn = new SetWithDB(Constants.updatePomodoro,
-				contactMap.get(Constants.TASK_ID_DB), Integer.toString(userID));
-		dbConn.execute();
-		Intent intent = new Intent(SingleTask.this, ListTasks.class);
-		startActivity(intent);
+        } catch (Exception e) {
+            Log.e("Error", "Error in: " + e.getMessage());
+            e.printStackTrace();
+        }
+    }
 
-	}
+    public void registerPomodoro(View view) {
+        SetWithDB dbConn = new SetWithDB(Constants.updatePomodoro,
+                contactMap.get(Constants.TASK_ID_DB), Integer.toString(userID));
+        dbConn.execute();
+        Intent intent = new Intent(SingleTask.this, ListTasks.class);
+        startActivity(intent);
 
-	public void setDone(View view) {
-		SetWithDB dbConn = new SetWithDB(Constants.setDone, contactMap.get(Constants.TASK_ID_DB),
-				Integer.toString(userID));
-		dbConn.execute();
-		Intent intent = new Intent(SingleTask.this, ListTasks.class);
-		startActivity(intent);
+    }
 
-	}
+    public void setDone(View view) {
+        SetWithDB dbConn = new SetWithDB(Constants.setDone, contactMap.get(Constants.TASK_ID_DB),
+                Integer.toString(userID));
+        dbConn.execute();
+        Intent intent = new Intent(SingleTask.this, ListTasks.class);
+        startActivity(intent);
 
-	private class SetWithDB extends AsyncTask<Void, Void, Boolean> {
-		String url = "http://ludste.synology.me/TaskTracker/index.php";
+    }
 
-		String method;
-		String taskID;
-		String userID;
+    public void remove(View view) {
+        SetWithDB dbConn = new SetWithDB(Constants.remove, contactMap.get(Constants.TASK_ID_DB),
+                Integer.toString(userID));
+        dbConn.execute();
+        Intent intent = new Intent(SingleTask.this, ListTasks.class);
+        startActivity(intent);
 
-		public SetWithDB(String method, String taskID, String userID) {
-			this.method = method;
-			this.taskID = taskID;
-			this.userID = userID;
-		}
+    }
 
-		@Override
-		protected Boolean doInBackground(Void... params) {
-			ArrayList<NameValuePair> nameValuePairs = new ArrayList<NameValuePair>();
-			nameValuePairs.add(new BasicNameValuePair(Constants.METHOD, method));
-			nameValuePairs.add(new BasicNameValuePair(Constants.TASK_ID_DB, taskID));
-			nameValuePairs.add(new BasicNameValuePair(Constants.USER_ID_DB, userID));
-			ServiceHandler serviceHandler = new ServiceHandler();
-			String jsonStr = serviceHandler
-					.makeServiceCall(url, ServiceHandler.GET, nameValuePairs);
-			if (method.equals(Constants.getCollab)) {
-				try {
-					JSONObject all = new JSONObject(jsonStr);
-					collaborators = all.getString("data");
+    public void startPomodoro(View view) {
+        long time = 1000 * 5;
+        startTimer(time, Constants.TIME_POMODORO);
+    }
 
-				}
-				catch (Exception e) {
-					e.printStackTrace();
-				}
-			}
-			if (jsonStr.contains(method))
-				return true;
-			return false;
+    public void startLongBreak(View view) {
+        long time = 1000 * 60 * 10;
+        startTimer(time, Constants.TIME_LONG);
+    }
 
-		}
+    public void startShortBreak(View view) {
 
-		protected void onPostExecute(Boolean result) {
-			if (!method.equals(Constants.getCollab)) {
-				if (result) {
-					new AlertDialog.Builder(SingleTask.this).setMessage("It has now been updated")
-							.setNeutralButton("OK", new DialogInterface.OnClickListener() {
-								public void onClick(DialogInterface dialog, int which) {
+        long time = 1000 * 60 * 5;
+        startTimer(time, Constants.TIME_SHORT);
+    }
 
-								}
-							}).show();
-				}
-				else {
-					System.err.println("Show warning dialog");
-					new AlertDialog.Builder(SingleTask.this)
-							.setMessage("Error in connection with database, please try again")
-							.setNeutralButton("OK", new DialogInterface.OnClickListener() {
-								public void onClick(DialogInterface dialog, int which) {
+    public void startTimer(long time, final String type) {
 
-								}
-							}).show();
-				}
-			}
-		}
+        final long millisGoal = System.currentTimeMillis() + time;
+        final TextView timer = (TextView) findViewById(R.id.single_task_TV_pomodoro_clock);
+        Handler timerHandler = new Handler();
+        Runnable timerRunnable = new Runnable() {
 
-	}
+            @Override
+            public void run() {
+                long millis = millisGoal - System.currentTimeMillis();
+                if (millis <= 0) {
+                    if (type.equals(Constants.TIME_POMODORO)){
+                        registerPomodoro(null);
+                    }
+                    return;
+                }
+                int seconds = (int) (millis / 1000);
+                int minutes = seconds / 60;
+                seconds = seconds % 60;
+
+                timer.setText(String.format("%d:%02d", minutes, seconds));
+
+                timer.postDelayed(this, 500);
+            }
+        };
+        timerHandler.postDelayed(timerRunnable, 0);
+
+    }
+
+
+    private class SetWithDB extends AsyncTask<Void, Void, Boolean> {
+        String url = "http://ludste.synology.me/TaskTracker/index.php";
+
+        String method;
+        String taskID;
+        String userID;
+
+        public SetWithDB(String method, String taskID, String userID) {
+            this.method = method;
+            this.taskID = taskID;
+            this.userID = userID;
+        }
+
+        @Override
+        protected Boolean doInBackground(Void... params) {
+            ArrayList<NameValuePair> nameValuePairs = new ArrayList<NameValuePair>();
+            nameValuePairs.add(new BasicNameValuePair(Constants.METHOD, method));
+            nameValuePairs.add(new BasicNameValuePair(Constants.TASK_ID_DB, taskID));
+            nameValuePairs.add(new BasicNameValuePair(Constants.USER_ID_DB, userID));
+            ServiceHandler serviceHandler = new ServiceHandler();
+            String jsonStr = serviceHandler
+                    .makeServiceCall(url, ServiceHandler.GET, nameValuePairs);
+            if (method.equals(Constants.getCollab)) {
+                try {
+                    JSONObject all = new JSONObject(jsonStr);
+                    collaborators = all.getString("data");
+
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+            if (jsonStr.contains(method))
+                return true;
+            return false;
+
+        }
+
+        protected void onPostExecute(Boolean result) {
+            if (!method.equals(Constants.getCollab)) {
+                if (result) {
+                    new AlertDialog.Builder(SingleTask.this).setMessage("It has now been updated")
+                            .setNeutralButton("OK", new DialogInterface.OnClickListener() {
+                                public void onClick(DialogInterface dialog, int which) {
+
+                                }
+                            }).show();
+                } else {
+                    System.err.println("Show warning dialog");
+                    new AlertDialog.Builder(SingleTask.this)
+                            .setMessage("Error in connection with database, please try again")
+                            .setNeutralButton("OK", new DialogInterface.OnClickListener() {
+                                public void onClick(DialogInterface dialog, int which) {
+
+                                }
+                            }).show();
+                }
+            }
+        }
+
+    }
 }
